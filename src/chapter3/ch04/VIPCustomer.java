a91c3fa6a7c9c54dfeb1240a020e31db47dd161e
JeongJinUng <jinung.dev@gmail.com>
2021-12-12 23:54:06 +0900

    다형성 실습

---
+++ src/chapter3/ch04/VIPCustomer.java
@@ -0,0 +1,52 @@
+package chapter3.ch04;
+
+public class VIPCustomer extends Customer {
+
+    double salesRatio;
+    private String agentID;
+
+    public VIPCustomer(int customerID, String customerName) {
+        super(customerID, customerName);
+
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+
+        System.out.println("VIPCustomer(int, String) call");
+    }
+
+    @Override
+    public int calcPrice(int price) {
+
+        bonusPoint += price * bonusRatio;
+        price -= (int)(price * salesRatio);
+        return price;
+    }
+
+    /*
+    public VIPCustomer() {
+//        super(); // 없으면 자동으로 상위 클래스의 생성자를 컴파일러가 넣어준다.
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+        customerGrade = "VIP";
+
+        System.out.println("VIPCustomer() call");
+    }
+    */
+
+    public double getSalesRatio() {
+        return salesRatio;
+    }
+
+    public void setSalesRatio(double salesRatio) {
+        this.salesRatio = salesRatio;
+    }
+
+    public String getAgentID() {
+        return agentID;
+    }
+
+    public void setAgentID(String agentID) {
+        this.agentID = agentID;
+    }
+}
